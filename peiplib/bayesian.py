73f0c760ecb299af5e9d2257b97447fb7a83b784
Nima Nooshiri <nima.nooshiri@gmail.com>
2018-02-19 08:24:04 +0100

    bayesian: correlation matrix

--- peiplib/bayesian.py
+++ peiplib/bayesian.py
@@ -2,6 +2,8 @@ import numpy as np
 from numpy import linalg as nla
 from scipy import linalg as sla
 
+from peiplib.util import acorr
+
 
 def bayes(G, mprior, covm, d, covd):
     """
@@ -28,6 +30,9 @@ def bayes(G, mprior, covm, d, covd):
     covmp : array-like
         Posterior covariance matrix.
     """
+
+    mprior = np.ravel(mprior)
+    d = np.ravel(d)
 
     covm_inv = nla.inv(covm)
     covd_inv = nla.inv(covd)
@@ -77,3 +82,53 @@ def simmvn(mu, cov):
 
     R = sla.cholesky(cov, lower=False)
     return np.dot(R.T, np.random.randn(mu.size)) + mu
+
+
+def corrmat(M, corrlen, want_corrfun=False):
+    """
+    Construct a correlation matrix with columns
+
+        R_{i,.} = shift(a_j, i)
+
+    where ``a_j`` is the desired sequence of parameter correlations,
+    with a zero lag correlation of 1. For more details see eq. (11.35)
+    in Aster, Borchers and Thurber (2011).
+
+    Parameters
+    ----------
+    M : int
+        Number of rows (and columns) in M-by-M output.
+
+    corrlen : int
+        Correlation length (a correlation of ``1/e`` at a lag of
+        approximately ``corrlen``).
+
+    want_corrfun : bool, (optional, default: False)
+        Whether to return correlation function array in output.
+
+    Returns
+    -------
+    R : array-like
+        ``M-by-M`` array of correlation matrix.
+
+    cfun : array-like (optional)
+        Array of correlation function of length ``2M``.
+    """
+
+    # Start with a triangular root kernel
+    ckern = np.zeros(2*M, dtype=np.float)
+    # ckern[M-corrlen-1:M+corrlen] = np.bartlett(2*corrlen + 1)
+    ckern[M-corrlen-2:M+corrlen+1] = np.bartlett(2*corrlen + 3)
+
+    # Auto-correlation of root kernel to define a uniform correlation structure
+    cfun = acorr(ckern, maxlag=M, normed=True, want_lags=False)
+
+    # Populate the correlation matrix
+    R = np.zeros((M, M), dtype=np.float)
+    for i in range(M):
+        c = np.roll(cfun, i+1+M)
+        R[:, i] = c[:M]
+
+    if want_corrfun:
+        return R, cfun
+    return R
